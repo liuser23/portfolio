440044d34c7241405008ccbc23072a472f9d35be
liuser23 <joshua_liu@brown.edu>
2023-08-23 14:52:44 -0500

    update portfolio
    Former-commit-id: 67ba729f18645a8167c64a16f313abf28e03fb71

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import Typewriter from "typewriter-effect/dist/core";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+let heroTypewriter = document.getElementById("hero-typewriter");
+
+let typewriter = new Typewriter(heroTypewriter, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2000)
+  .typeString(
+    'I\'m a <span class="text-color-between">full stack developer</span>.'
+  )
+  .pauseFor(1000)
+  .deleteChars(21)
+  .pauseFor(600)
+  .typeString('<span class="text-color-between">student</span>.')
+  .pauseFor(1000)
+  .deleteAll()
+  .pauseFor(600)
+  .typeString("Thanks for visiting!")
+  .pauseFor(1000)
+  .start();
